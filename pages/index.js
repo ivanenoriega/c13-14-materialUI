6499cbdf236f230d566675de13d412d039b6a110
Barbi Torres <barbi.mtorres@gmail.com>
2023-03-17 00:18:05 +0100

    class14-completed

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,7 @@ import SelectStyle from "../src/components/Select";
 import RadioButton from "../src/components/RadioButton";
 import CheckBox from "../src/components/CheckBox";
 import BasicModal from "../src/components/Modal.jsx";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+
 
 const Home = () => {
   return (
@@ -57,33 +52,6 @@ const Home = () => {
           </Box>
         </Paper>
      
-      {/* <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Nombre</TableCell>
-              <TableCell>Apellido</TableCell>
-              <TableCell>Género</TableCell>
-              <TableCell>Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>Barbi</TableCell>
-              <TableCell>Torres</TableCell>
-              <TableCell>Femenino</TableCell>
-              <TableCell>barbi@gmail.com</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Barbi</TableCell>
-              <TableCell>Torres</TableCell>
-              <TableCell>Femenino</TableCell>
-              <TableCell>barbi@gmail.com</TableCell>
-            </TableRow>
-            
-          </TableBody>
-        </Table>
-      </TableContainer> */}
   </Box>
   );
 };
