f29d54ad5a6e298428dccdb5bc0eddfafa491c05
Barbi Torres <barbi.mtorres@gmail.com>
2023-03-17 00:03:21 +0100

    class14

--- pages/index.js
+++ pages/index.js
@@ -12,29 +12,79 @@ import SelectStyle from "../src/components/Select";
 import RadioButton from "../src/components/RadioButton";
 import CheckBox from "../src/components/CheckBox";
 import BasicModal from "../src/components/Modal.jsx";
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 const Home = () => {
   return (
-    <Box sx={{ maxWidth: 800 }}>
-      <Paper
-        elevation={1}
-        sx={{ p: "32px", display: "flex", flexDirection: "column", gap: 3 }}
-      >
-        <SelectStyle />
-        <RadioButton />
-        <CheckBox />
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
+      <Box sx={{ maxWidth: 800 }}>
+        <Paper
+          elevation={1}
+          sx={{  p: "32px", display: "flex", flexDirection: "column", gap: 3}}
         >
-          <BasicModal />
-        </Box>
-      </Paper>
-    </Box>
+          <Grid item xs={12}>
+            <TextField
+              id="outlined-basic"
+              label="Nombre"
+              variant="outlined"
+              sx={{ width: 1 }}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              id="outlined-basic"
+              label="Apellido"
+              variant="outlined"
+              sx={{ width: 1 }}
+            />
+          </Grid>
+          <SelectStyle />
+          <RadioButton />
+          <CheckBox />
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+            }}
+          >
+            <BasicModal />
+          </Box>
+        </Paper>
+     
+      {/* <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Apellido</TableCell>
+              <TableCell>Género</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Barbi</TableCell>
+              <TableCell>Torres</TableCell>
+              <TableCell>Femenino</TableCell>
+              <TableCell>barbi@gmail.com</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Barbi</TableCell>
+              <TableCell>Torres</TableCell>
+              <TableCell>Femenino</TableCell>
+              <TableCell>barbi@gmail.com</TableCell>
+            </TableRow>
+            
+          </TableBody>
+        </Table>
+      </TableContainer> */}
+  </Box>
   );
 };
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import GridData from './Table';
 
@@ -10,7 +9,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '65%',
+  width: '800px',
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
